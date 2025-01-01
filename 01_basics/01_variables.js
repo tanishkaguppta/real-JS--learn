d307ad74fd36db3da633f2201032049886ae78bd
tanishkaguppta <tanishkagupt2000@gmail.com>
2025-01-01 13:55:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const  accountid = 1541   // no chnge in varibles
+var accountname = "anu"        //
+let accountpswd = "463254"
+accountcity = "etah"
+
+//accountid = 47666;   // const no change in variables
+
+accountname = "manu"
+accountpswd = "362786";
+accountemail = "manu67@gmail.com";
+let accountstate;
+
+
+console.log(accountid);
+
+/*
+prefer not to use var 
+because of issues in block scope and functional scope
+ */
+
+
+console.table([accountid,accountname,accountpswd,accountcity,accountemail,accountstate]);
+
+
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,38 @@
+"use strict" ;     // treat all js code as a newer  version
+
+
+// alert(3+3);  // we are using nodejs, not browser
+
+
+console.log(3+32);     // code readability should be high
+
+console.log("tanu");
+
+
+let name = "tanu";
+let age = 23;
+let isloggedIn = false
+let state = null;
+
+
+
+//datatyes-- 2 types--
+//1> primitive
+
+//number =>  2 to power 53
+//bigint
+//string => ""
+//boolean => true,false
+//null => standalone value
+//undefined => that means abhi value assign nhi hui hai
+//symbol => iska use react me jyada hoga jab hm components banayenge , jab hame identify krna hai ye wla ye ya wo wala component unique hai.
+  //basically jab hame baat krni ho unique ke baare me tb symbol ka use krte hai
+
+// 2> references datatypes--
+//object--
+//array--
+//function--
+
+
+console.log(typeof undefined);   //undefined
+console.log(typeof null);     //object  
